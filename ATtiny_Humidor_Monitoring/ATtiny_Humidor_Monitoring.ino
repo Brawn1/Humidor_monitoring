c6988890b1161e14c6448b6437578baaf9e9d353
Guenter Bailey <office@bailey-solution.com>
2017-02-02 22:05:54 +0100

    Programm mit ShiftRegister und virtualwire erweitert

--- ATtiny_Humidor_Monitoring/ATtiny_Humidor_Monitoring.ino
+++ ATtiny_Humidor_Monitoring/ATtiny_Humidor_Monitoring.ino
@@ -19,6 +19,10 @@
  * PB3 = 433MHz Sender
  * PB4 = DHT11 Sensor
  * 
+ * shiftRegister Belegung:
+ * out1 = schieber / oeffner
+ * out2 = Luefter
+ * 
  * Info:
  * Falls ein anderer Arduino Controller verwendet wird, muss man die Pins anpassen.
  * 
@@ -54,91 +58,158 @@
  * EINES DELIKTES ODER ANDERS IM ZUSAMMENHANG MIT DER SOFTWARE ODER SONSTIGER VERWENDUNG DER SOFTWARE ENTSTANDEN.
  * 
  */
-//attiny85
+#include<stdlib.h>
+//VirtualWire lib
+#include <VirtualWire.h>
+#undef int
+#undef abs
+#undef double
+#undef float
+#undef round
+
+int txPin = 3; //ueber Pin 3 die Daten senden
+
+//attiny85 shiftregister
 /*
 #define latchPin PB2
 #define clockPin PB0
 #define dataPin PB1
+int dhtPin = PB4; //DHT11 Pin
 */
-//arduino
+//arduino shiftregister
 #define latchPin A1
 #define clockPin A0
 #define dataPin A2
-//manchester lib
-#include <Manchester.h>
-#define TX_PIN  5  //uebertragungs Pin
+// shiftregister
+byte reg = 0;
+
+//DHT11 und 22 lib
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#define dhtPin 5 //DHT Pin
+// Uncomment the type of sensor in use:
+//#define DHTTYPE DHT11 // DHT 11 
+#define DHTTYPE DHT22 // DHT 22 (AM2302)
+//#define DHTTYPE DHT21 // DHT 21 (AM2301)
+DHT_Unified dht(dhtPin, DHTTYPE);
 
 //Pausen zwischen den Messungen in Millisekunden
-unsigned long mtime = 10000;
+unsigned long stime = 10000;
+unsigned long mtime = 5000;
 
-/*
- * Verschiedene Uebertragungsgeschwindigkeiten sind moeglich.
- * Variiert von der Distanz und Empfangsqualitaet
- * 
- * Bei den Tests hat am besten die MAN_1200 Funktioniert.
- * 
- * MAN_300 0
- * MAN_600 1
- * MAN_1200 2
- * MAN_2400 3
- * MAN_4800 4
- * MAN_9600 5
- * MAN_19200 6
- * MAN_38400 7
- */
+// boolean felder
+boolean fan1 = false;
 
 void setup() {
   //Serial.begin(115200);
-
-  //manchester lib
-  man.workAround1MhzTinyCore(); //patch fuer 1Mhz ATtinyx4/x5
-  man.setupTransmit(TX_PIN, MAN_1200);
-  
+  // Initialise the IO and ISR
+  vw_set_ptt_inverted(true); // Required for RF Link module
+  vw_setup(1200); // Bits per sec
+  vw_set_tx_pin(txPin); // pin 3 is used as the transmit data out into the TX Link module, change this as per your needs
   //shiftregister
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
+  //init DHT device
+  dht.begin();
+  sensor_t sensor;
 }
 
-char msg[] = "adsfED";
-
-byte count = 1;
-unsigned long task1 = 0;
+unsigned long task1, task2 = 0;
 void loop() {
   unsigned long currmillis = millis();
   
-  // Wenn die Zeit (worktime) kleiner als die Vergangene Zeit ist, schalte ab.
-  if ((unsigned long)(currmillis - task1) >= mtime || (currmillis == 1000)) {
-    uint16_t msglen = sizeof(msg)-1;
-    man.transmitArray(msglen, msg);
+  // Wenn die Zeit (worktime) kleiner als die Vergangene Zeit ist, Sende eine Nachricht.
+  if ((unsigned long)(currmillis - task1) >= stime || (currmillis == 1000)) {
+    send_msg("Testnachricht_1");
+    compute_msg(temp_sensor_id(), get_temp(), hum_sensor_id(), get_hum());
     task1 = millis();
   }
-  /*
-  //if ((unsigned long)(currmillis - task1) == 1000) {
-  //rf_receive();
-  //}
-  */
-}
 
-/*
-void rf_receive() {
-  if (mySwitch.available()) {
-    
-    int value = mySwitch.getReceivedValue();
-    
-    if (value == 0) {
-      Serial.print("Unknown encoding");
+  // checke die Luftfeuchtigkeit und wenn zu niedrig schalte Luefter ein.
+  if ((unsigned long)(currmillis - task2) >= mtime) {
+    if ((float)(get_hum() <= 70.0)) {
+      // switch ventilator on
+      if (!fan1) {
+        //oeffne Luefter schlitz
+        int i = 1;
+        bitSet(reg, i);
+        updateShiftRegister();
+        delay(10);
+        //switch fan1 on
+        i = i + 1;
+        bitSet(reg, i);
+        updateShiftRegister();
+        delay(60);
+        fan1 = true;
+      }
     } else {
-      Serial.print("Received ");
-      Serial.print( mySwitch.getReceivedValue() );
-      Serial.print(" / ");
-      Serial.print( mySwitch.getReceivedBitlength() );
-      Serial.print("bit ");
-      Serial.print("Protocol: ");
-      Serial.println( mySwitch.getReceivedProtocol() );
+      if (fan1) {
+        // switch fan1 off und schliesse den Schlitz
+        reg = 0;
+        updateShiftRegister();
+        delay(60);
+        fan1 = false;
+      }
     }
-
-    mySwitch.resetAvailable();
+    task2 = millis();
   }
+  
 }
-*/
+
+void updateShiftRegister() {
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, reg);
+   digitalWrite(latchPin, HIGH);
+}
+
+char temp_sensor_id() {
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  return sensor.sensor_id;
+}
+
+char hum_sensor_id() {
+  sensor_t sensor;
+  dht.humidity().getSensor(&sensor);
+  return sensor.sensor_id;
+}
+
+float get_temp() {
+  // Read temperature as Celsius (the default)
+  sensors_event_t event;  
+  dht.temperature().getEvent(&event);
+  return event.temperature;
+}
+
+float get_hum() {
+  // Read humidity
+  sensors_event_t event;  
+  dht.humidity().getEvent(&event);
+  return event.relative_humidity;
+}
+
+void compute_msg(char temp_uuid, float temp, char hum_uuid, float hum){
+  /*
+   * compute message for sending data
+   * dtostrf(FLOAT,WIDTH,PRECSISION,BUFFER);
+   */
+   char str;
+   str += temp_uuid;
+   str += dtostrf(temp,5,2,7);
+   str += hum_uuid;
+   str += dtostrf(hum,5,2,7);
+   //Serial.println(str);
+   send_msg(str);
+}
+
+void send_msg(char message) {
+  /*
+   * Sending data over 433MHz Sender
+   */
+  const char *msg = message; // this is your message to send
+  vw_send((uint8_t *)msg, strlen(msg));
+  vw_wait_tx(); // Wait for message to finish
+}
+
